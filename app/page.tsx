bca4346f13251548d00fd9da5830845497a176a2
valentinpradobasco-ai <valentinpradobasco@gmail.com>
2026-08-28 18:50:38 -0300

    Delete app directory

--- app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { restaurantConfig } from "@/config/restaurant.config";
-import { MenuExperience } from "@/components/MenuExperience";
-
-export default function Home() {
-  return <MenuExperience config={restaurantConfig} />;
-}
